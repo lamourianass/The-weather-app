4f19695724273aee9fdaf91e73e1e39f9a56d5d9
Anass Lamouri <anasslamouri@icloud.com>
2025-08-26 15:19:40 +0100

    update #js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const cityInput = document.querySelector('.city-input')
 const searchBtn = document.querySelector('.search-btn')
 const notFoundSection = document.querySelector('.not-found')
+const searchCitySection = document.querySelector('.search-city')
+const weatherInfoSection = document.querySelector('.weather-info')
 const apiKey = "fcc7518e67cb0ca5968f2ef774337981"
+
+const countryTxt = document.querySelector('.country-txt')
+const tempTxt =document.querySelector('.temp-txt')
+const conditionTxt =document.querySelector('.condition-txt')
+const humidityValueTxt =document.querySelector('.humidity-value-txt')
+const windValueTxt =document.querySelector('.wind-value-txt')
+const weatherSummaryImg =document.querySelector('.weather-summary-img')
+const currentDateTxt = document.querySelector('.current-date-txt')
+
 
 searchBtn.addEventListener('click', () => {
   if (cityInput.value.trim() != '') {
@@ -35,8 +46,19 @@ async function updateWeatherInfo(city) {
     showDisplaySection(notFoundSection)
     return
   }
+  console.log(weatherData);
+  const {
+    name: country,
+    main: { temp, humidity },
+    weather: [{ id, main }],
+    wind: { speed }
+  } = weatherData
+
+  showDisplaySection(weatherInfoSection)
 }
 
 function showDisplaySection(section) {
-
+  [weatherInfoSection, searchCitySection, notFoundSection]
+    .forEach(section => section.style.display = 'none')
+  section.style.display = 'flex'
 }
